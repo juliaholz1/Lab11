f40e362899e62e601f7bd012f3577114010968dd
juliaholz1 <juliaholz1@ymail.com>
2021-04-27 19:48:17 -0500

    Added another comment

---
+++ fiboRecursiveInit.m
@@ -0,0 +1,8 @@
+function [f] = fiboRecursiveInit(n)
+    global count;
+    count = 0;
+    f = fiboRecursive(n);
+    fprintf('It took %d function calls to calculate that the %dth Fibonacci number is %d.\n',count,f);
+end
+%this is my comment for this
+%another comment again
